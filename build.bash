782ca694e58613f37cced10b2c740f7204e6d863
Tim Visher <tim.visher@gmail.com>
2010-02-09 21:05:07 -0500

    Build Script Working Correctly. EOM

--- build.bash
+++ build.bash
@@ -1,12 +1,17 @@
-if [ -d build] ; then
+if [ -d build ] ; then
     rm -r build
 fi
 mkdir build
 cp literatureReview.tex build/
 cp bibliography.bib build/
 cd build/
+mkdir latex
+mv literatureReview.tex bibliography.bib latex
+cd latex
+latex literatureReview.tex
 latex literatureReview.tex
 bibtex literatureReview.aux
 latex literatureReview.tex
 pdflatex literatureReview.tex
+mv literatureReview.pdf ../
 
